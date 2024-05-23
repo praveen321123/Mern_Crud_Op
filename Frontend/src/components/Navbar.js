720342ad48f47540f5e719c6ecbc457ba8d33446
Praveen S <praveens321123@gmail.com>
2024-05-23 11:01:17 +0530

    Intendation

--- Frontend/src/components/Navbar.js
+++ Frontend/src/components/Navbar.js
@@ -7,6 +7,15 @@ const Navbar = () => {
 
   return (
     <>
+      <nav>
+        <ul class="nav justify-content-center">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="https://www.achieversit.com/">
+              <img src="https://www.achieversit.com/assets/images/logo-white.png" alt="ACIT_Logo"/>
+            </a>
+          </li>
+        </ul>
+      </nav>
       <h1 className="text-center my-4 text-primary">Project Management</h1>
       <p className="text-center lead">{`Currently ${tasksList.length} task(s) pending`}</p>
       {
